24a247f08e16289b30f79d1bb0a41c46a7278340
Rajat Singhal <37938604+rajat2004@users.noreply.github.com>
2018-09-27 13:09:49 +0530

    Updated entrypoint.sh

--- ROS/entrypoint.sh
+++ ROS/entrypoint.sh
@@ -1,1 +1,11 @@
+#!/bin/bash
+
+useradd --shell /bin/bash -c "" -m $USERNAME
+
+export HOME=/bome/$USERNAME
+export PATH=/home/$USERNAME/bin:/usr/local/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/home/$USERNAME/local/bin/:/usr/local
+
+usermod -aG sudo $USERNAME
+echo "$USERNAME:test" | chpasswd
+
 source /root/ros_catkin_ws/install_isolated/setup.bash
