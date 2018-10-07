a6fedf299dfe48bb9d81979fb4ba39126c794cb5
Rajat Singhal <37938604+rajat2004@users.noreply.github.com>
2018-10-07 07:01:29 +0530

    Updated entrypoit.sh

--- ROS/entrypoint.sh
+++ ROS/entrypoint.sh
@@ -2,10 +2,10 @@
 
 useradd --shell /bin/bash -c "" -m $USERNAME
 
-export HOME=/bome/$USERNAME
-export PATH=/home/$USERNAME/bin:/usr/local/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/home/$USERNAME/.local/bin/:/usr/local
+export HOME=/home/$USERNAME
+export PATH=/home/$USERNAME/bin:/usr/local/bin:/usr/local/sbin:/usr/sbin:/usr/bin:/sbin:/bin:/home/$USERNAME/.local/bin/:/usr/local
 
 usermod -aG sudo $USERNAME
 echo "$USERNAME:test" | chpasswd
 
-source /root/ros_catkin_ws/install_isolated/setup.bash
+exec /usr/local/bin/gosu $USERNAME "$@"
